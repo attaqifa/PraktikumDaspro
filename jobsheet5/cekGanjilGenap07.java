4ae50daa6cf05b2ca7647352edcfb063b4b9f2d4
Fadhil <attaqifa@gmail.com>
2025-10-05 23:20:40 +0700

    Tugas 2

---
+++ jobsheet5/cekGanjilGenap07.java
@@ -0,0 +1,19 @@
+package jobsheet5;
+import java.util.Scanner;
+
+public class cekGanjilGenap07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan angka: ");
+        int angka = input.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+
+        input.close();
+    }
+}
