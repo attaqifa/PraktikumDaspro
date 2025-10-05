6a8553099595655990b39df98f0e3222d3779e51
Fadhil <attaqifa@gmail.com>
2025-10-05 23:15:59 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS07.java
+++ jobsheet5/ifCetakKRS07.java
@@ -9,12 +9,21 @@ public class ifCetakKRS07 {
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
+        // Versi IF-ELSE (lama)
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
         } else {
             System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
+
+        // Versi Ternary Operator (baru)
+        String pesan = uktLunas 
+            ? "Pembayaran UKT terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA" 
+            : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+
+        System.out.println(pesan);
+
         sc.close();
     }
 }
